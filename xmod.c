4bbad6de72733958cbd80c658e4aa008d003f131
melisilva <55757043+melisilva@users.noreply.github.com>
2021-03-04 23:00:56 +0000

    Add files via upload
    Gets current permissions on a given file & converts them to octal.
    Checks if options were introduced and which ones.
    Working on way to program every single way letters can be inputed.

--- xmod.c
+++ xmod.c
@@ -3,6 +3,8 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <ctype.h>
+#include <cstring>
+#include <string.h>
 
 int xmod(mode_t mode,char*path){
 
@@ -34,16 +36,16 @@ int main(int argc,char *argv[]){ //, char*env
 
     //Put code bellow in xmod()-probably?
 
+    char *options;
+    int mode;
+    if(argv[1][0]=='-' && (argv[1][1]=='c' | argv[1][1]=='R' | argv[1][1]=='v')){ //Or argc==4
+       options=argv[1];
+       printf("%s\n",options);
+    }
+
     //Turn mode (when written in digits) to an octal number in order to call chmod function
-    /*int mode=atoi(argv[1]);
-    
-    int mode_u = mode / 100;
-    int mode_g = mode / 10 % 10;
-    int mode_o = mode % 10;
-    mode = (mode_u * 8 * 8) + (mode_g * 8) + mode_o; // Octal
-    */
-   if(isdigit(argv[1][0])){
-        int mode=strtol(argv[1],0,8);
+   else if(isdigit(argv[1][0])){
+        mode=strtol(argv[1],0,8);
 
         if(chmod(argv[2],mode)<0){
            printf("ERROR");
@@ -52,17 +54,120 @@ int main(int argc,char *argv[]){ //, char*env
    else{
        /*We need to know the current permissions in place because here we 
        are just adding or deleting permissions to certain people, we
-       don't change the current permissions for the other people*/
-       printf("Holo\n");
-       if(argv[1][0]=='-'){ //if mode==-r--rw----
-           printf("I do\n");
-       }
+       don't change the current permissions for the other people.
+       */
+
+       int size=strlen(argv[1]);
+       printf("%d\n",size);
        struct stat fs;
        stat(argv[2],&fs); //Gets current permission
-       printf("Current permissions: \n");
-       output_permissions(fs.st_mode); 
-   }
+       int mode_u=0,mode_g=0,mode_o=0;
+       
+       //User Permission
+       if(fs.st_mode & S_IRUSR){
+           mode_u+=4;
+       }
+       if(fs.st_mode & S_IWUSR){
+           mode_u+=2;
+       }
+       if(fs.st_mode & S_IXUSR){
+           mode_u+=1;
+       }
+       //Group Permission
+        if(fs.st_mode & S_IRGRP){
+           mode_g+=4;
+       }
+       if(fs.st_mode & S_IWGRP){
+           mode_g+=2;
+       }
+       if(fs.st_mode & S_IXGRP){
+           mode_g+=1;
+       }
 
+       //Others Permission
+        if(fs.st_mode & S_IROTH){
+           mode_o+=4;
+       }
+       if(fs.st_mode & S_IWOTH){
+           mode_o+=2;
+       }
+       if(fs.st_mode & S_IXOTH){
+           mode_o+=1;
+       }
+
+       /*Let's consider letters option first (u+w)
+       if(argv[1][0]=='u'){
+          int i=1;
+          while(i!=size-1){
+              if(argv[1][i]=='+'){
+                  i++;
+                  if(argv[1][i]=='r'){
+                      mode_u+=4;
+                  }
+                  if(argv[1][i]=='w'){
+                      mode_u+=2;
+                  }
+                  if(argv[1][i]=='x'){
+                      mode_u+=1;
+                  }
+                  i++;
+              }
+
+              if(argv[1][i]=='-'){
+                  i++;
+                  if(argv[1][i]=='r'){
+                      mode_u-=4;
+                  }
+                  if(argv[1][i]=='w'){
+                      mode_u-=2;
+                  }
+                  if(argv[1][i]=='x'){
+                      mode_u-=1;
+                  }
+                  i++;
+              }
+
+              if(argv[1][i]=='='){
+                  mode_u=0;
+                  i++;
+                  if(argv[1][i]=='r'){
+                      mode_u+=4;
+                  }
+                  if(argv[1][i]=='w'){
+                      mode_u+=2;
+                  }
+                  if(argv[1][i]=='x'){
+                      mode_u+=1;
+                  }
+                  i++;
+              }
+          }
+       }
+*/
+
+
+
+
+
+        char mode_str[3]={'0','0','0'};
+        mode_str[0]=mode_u+'0';
+        mode_str[1]=mode_g+'0';
+        mode_str[2]=mode_o+'0';
+
+        mode=strtol(mode_str,0,8);
+        printf("%s\n",mode_str);
+
+        if(chmod(argv[2],mode)<0){
+           printf("ERROR");
+    }
+
+
+
+
+       
+
+      
+   }
    //Symbolic Link check
    /*
 while ((ent = readdir(dir)) != NULL) {
