1e5003df60214c10bd16f7a0dcfffb788f19bdae
Melissa <up201905076@fe.up.pt>
2021-03-05 09:30:19 +0000

    Parse done + cleaner code

--- xmod.c
+++ xmod.c
@@ -6,35 +6,45 @@
 #include <cstring>
 #include <string.h>
 
-int xmod(mode_t mode,char*path){
-
-    return 0;
+void all_plus_read(int &mode_u_r,int &mode_g_r,int &mode_o_r){
+    mode_u_r=4;
+    mode_g_r=4;
+    mode_o_r=4;
 }
 
-void output_permissions(mode_t m) //Prints current permissions
-{
-    putchar( m & S_IRUSR ? 'r' : '-' );
-    putchar( m & S_IWUSR ? 'w' : '-' );
-    putchar( m & S_IXUSR ? 'x' : '-' );
-    putchar( m & S_IRGRP ? 'r' : '-' );
-    putchar( m & S_IWGRP ? 'w' : '-' );
-    putchar( m & S_IXGRP ? 'x' : '-' );
-    putchar( m & S_IROTH ? 'r' : '-' );
-    putchar( m & S_IWOTH ? 'w' : '-' );
-    putchar( m & S_IXOTH ? 'x' : '-' );
-    putchar('\n');
+void all_plus_write(int &mode_u_w,int &mode_g_w,int &mode_o_w){
+    mode_u_w=2;
+    mode_g_w=2;
+    mode_o_w=2;
 }
 
-int main(int argc,char *argv[]){ //, char*env
-    if(argc<3){ //chmod options permissions file_name
-                /*If no options are specified, chmod modifies the permissions of the file 
-                specified by file name to the permissions specified by permissions.
-                So it's possible to have only 3 arguments--->xmod, permissions, file_name*/
-        printf("Not enough arguments\n");
-        return 1;
-    }
+void all_plus_execute(int &mode_u_x,int &mode_g_x,int &mode_o_x){
+    mode_u_x=1;
+    mode_g_x=1;
+    mode_o_x=1;
+}
 
-    //Put code bellow in xmod()-probably?
+void all_minus_read(int &mode_u_r,int &mode_g_r,int &mode_o_r){
+    mode_u_r=0;
+    mode_g_r=0;
+    mode_o_r=0;
+}
+
+void all_minus_write(int &mode_u_w,int &mode_g_w,int &mode_o_w){
+    mode_u_w=0;
+    mode_g_w=0;
+    mode_o_w=0;
+}
+
+void all_minus_execute(int &mode_u_x,int &mode_g_x,int &mode_o_x){
+    mode_u_x=0;
+    mode_g_x=0;
+    mode_o_x=0;
+}
+
+
+
+int xmod(char *argv[]){
 
     char *options;
     int mode;
@@ -104,7 +114,7 @@ int main(int argc,char *argv[]){ //, char*env
        if(argv[1][0]=='u'){
           int i=1;
           while(i!=size){
-              if(argv[1][i]=='+'){
+              if(argv[1][i]=='+' || argv[1][i]=='='){
                   i++;
                   if(argv[1][i]=='r'){
                       mode_u_r=4;
@@ -120,32 +130,11 @@ int main(int argc,char *argv[]){ //, char*env
                   }
                   
               }
-            //Check how to stop from taking permission off of stuff that wasn't to begin with
               if(argv[1][i]=='-'){
                   i++;
                   if(argv[1][i]=='r'){
                       mode_u_r=0;
                       i++;
-                  }
-                  if(argv[1][i]=='w'){
-                      mode_u_w=2;
-                      i++;
-
-                  }
-                  if(argv[1][i]=='x'){
-                      mode_u_x=1;
-                      i++;
-
-                  }
-              }
-
-              if(argv[1][i]=='='){
-                  mode_u_r=0,mode_u_w=0,mode_u_x=0;
-                  i++;
-                  if(argv[1][i]=='r'){
-                      mode_u_r=4;
-                      i++;
-
                   }
                   if(argv[1][i]=='w'){
                       mode_u_w=2;
@@ -162,10 +151,10 @@ int main(int argc,char *argv[]){ //, char*env
        }
 
        //Group
-       if(argv[1][0]=='g'){
+       else if(argv[1][0]=='g'){
           int i=1;
           while(i!=size){
-              if(argv[1][i]=='+'){
+              if(argv[1][i]=='+' || argv[1][i]=='='){
                   i++;
                   if(argv[1][i]=='r'){
                       mode_g_r=4;
@@ -181,32 +170,11 @@ int main(int argc,char *argv[]){ //, char*env
                   }
                   
               }
-            //Check how to stop from taking permission off of stuff that wasn't to begin with
               if(argv[1][i]=='-'){
                   i++;
                   if(argv[1][i]=='r'){
                       mode_g_r=4;
                       i++;
-                  }
-                  if(argv[1][i]=='w'){
-                      mode_g_w=2;
-                      i++;
-
-                  }
-                  if(argv[1][i]=='x'){
-                      mode_g_x=1;
-                      i++;
-
-                  }
-              }
-
-              if(argv[1][i]=='='){
-                  mode_g_r=0,mode_g_w=0,mode_g_x=0;
-                  i++;
-                  if(argv[1][i]=='r'){
-                      mode_g_r=4;
-                      i++;
-
                   }
                   if(argv[1][i]=='w'){
                       mode_g_w=2;
@@ -223,10 +191,10 @@ int main(int argc,char *argv[]){ //, char*env
        }
 
        //Others
-       if(argv[1][0]=='o'){
+       else if(argv[1][0]=='o'){
           int i=1;
           while(i!=size){
-              if(argv[1][i]=='+'){
+              if(argv[1][i]=='+' || argv[1][i]=='='){
                   i++;
                   if(argv[1][i]=='r'){
                       mode_o_r=4;
@@ -242,32 +210,11 @@ int main(int argc,char *argv[]){ //, char*env
                   }
                   
               }
-            //Check how to stop from taking permission off of stuff that wasn't to begin with
               if(argv[1][i]=='-'){
                   i++;
                   if(argv[1][i]=='r'){
                       mode_o_r=4;
                       i++;
-                  }
-                  if(argv[1][i]=='w'){
-                      mode_o_w=2;
-                      i++;
-
-                  }
-                  if(argv[1][i]=='x'){
-                      mode_o_x=1;
-                      i++;
-
-                  }
-              }
-
-              if(argv[1][i]=='='){
-                  mode_o_r=0,mode_o_w=0,mode_o_x=0;
-                  i++;
-                  if(argv[1][i]=='r'){
-                      mode_o_r=4;
-                      i++;
-
                   }
                   if(argv[1][i]=='w'){
                       mode_o_w=2;
@@ -284,167 +231,88 @@ int main(int argc,char *argv[]){ //, char*env
        }
 
        //All
-       if(argv[1][0]=='a'){
+       else if(argv[1][0]=='a'){
           int i=1;
           while(i!=size){
-              if(argv[1][i]=='+'){
+              if(argv[1][i]=='+' || argv[1][i] == '='){
                   i++;
                   if(argv[1][i]=='r'){
-                      mode_u_r=4;
-                      mode_g_r=4;
-                      mode_o_r=4;
+                      all_plus_read(mode_u_r,mode_g_r,mode_o_r);
                       i++;
                   }
                   if(argv[1][i]=='w'){
-                      mode_u_w=2;
-                      mode_g_w=2;
-                      mode_o_w=2;
+                      all_plus_write(mode_u_w,mode_g_w,mode_o_w);
                       i++;
                   }
                   if(argv[1][i]=='x'){
-                      mode_u_x=1;
-                      mode_g_x=1;
-                      mode_o_x=1;
+                      all_plus_execute(mode_u_x,mode_g_x,mode_o_x);
                       i++;
                   }
                   
               }
-            //Check how to stop from taking permission off of stuff that wasn't to begin with
               if(argv[1][i]=='-'){
                   i++;
                   if(argv[1][i]=='r'){
-                      mode_u_r=0;
-                      mode_g_r=0;
-                      mode_o_r=0;
+                      all_minus_read(mode_u_r,mode_g_r,mode_o_r);
                       i++;
                   }
                   if(argv[1][i]=='w'){
-                      mode_u_w=0;
-                      mode_g_w=0;
-                      mode_o_w=0;
+                      all_minus_write(mode_u_w,mode_g_w,mode_o_w);
                       i++;
 
                   }
                   if(argv[1][i]=='x'){
-                      mode_u_x=0;
-                      mode_g_x=0;
-                      mode_o_x=0;
-                      i++;
-
-                  }
-              }
-
-              if(argv[1][i]=='='){
-                  mode_u_r=0,mode_u_w=0,mode_u_x=0,mode_g_r=0,mode_g_w=0,mode_g_x=0,mode_o_r=0,mode_o_w=0,mode_o_x=0;
-                  i++;
-                  if(argv[1][i]=='r'){
-                      mode_u_r=4;
-                      mode_g_r=4;
-                      mode_o_r=4;                     
-                      i++;
-
-                  }
-                  if(argv[1][i]=='w'){
-                      mode_u_w=2;
-                      mode_g_w=2;
-                      mode_o_w=2;
-                      i++;
-
-                  }
-                  if(argv[1][i]=='x'){
-                      mode_u_x=1;
-                      mode_g_x=1;
-                      mode_o_x=1;
-                      i++;
+                    all_minus_execute(mode_u_x,mode_g_x,mode_o_x);
+                    i++;
 
                   }
               }
           }
        }
 
-       //No letter: starting with -
-       if(argv[1][0]=='-'){
-           int i=1;
+       //No letter
+       else{
+           int i=0;
            while(i!=size){
+            if(argv[1][i]=='-'){
+                i++;
                 if(argv[1][i]=='r'){
-                      mode_u_r=0;
-                      mode_g_r=0;
-                      mode_o_r=0;
+                     all_minus_read(mode_u_r,mode_g_r,mode_o_r);
                       i++;
                   }
                   if(argv[1][i]=='w'){
-                      mode_u_w=0;
-                      mode_g_w=0;
-                      mode_o_w=0;
+                      all_minus_write(mode_u_w,mode_g_w,mode_o_w);
                       i++;
 
                   }
                   if(argv[1][i]=='x'){
-                      mode_u_x=0;
-                      mode_g_x=0;
-                      mode_o_x=0;
+                      all_minus_execute(mode_u_x,mode_g_x,mode_o_x);
                       i++;
 
                   }
            }
-
-       }
-
-       //No letter starting with +
-       if(argv[1][0]=='+'){
-          int i=1;
-          while(i!=size){
+           if(argv[1][i]=='+' || argv[1][i] == '='){
+                  i++;
                   if(argv[1][i]=='r'){
-                      mode_u_r=4;
-                      mode_g_r=4;
-                      mode_o_r=4;
+                      all_plus_read(mode_u_r,mode_g_r,mode_o_r);
                       i++;
                   }
                   if(argv[1][i]=='w'){
-                      mode_u_w=2;
-                      mode_g_w=2;
-                      mode_o_w=2;
+                      all_plus_write(mode_u_w,mode_g_w,mode_o_w);
                       i++;
                   }
                   if(argv[1][i]=='x'){
-                      mode_u_x=1;
-                      mode_g_x=1;
-                      mode_o_x=1;
+                      all_plus_execute(mode_u_x,mode_g_x,mode_o_x);
                       i++;
                   }
                   
               }
+          
+
+       }
        }
 
-       //No letter starting with =
-       if(argv[1][0]=='='){
-           int i=1;
-           while(i!=size){
-               mode_u_r=0,mode_u_w=0,mode_u_x=0,mode_g_r=0,mode_g_w=0,mode_g_x=0,mode_o_r=0,mode_o_w=0,mode_o_x=0;
-                  if(argv[1][i]=='r'){
-                      mode_u_r=4;
-                      mode_g_r=4;
-                      mode_o_r=4;                     
-                      i++;
-
-                  }
-                  if(argv[1][i]=='w'){
-                      mode_u_w=2;
-                      mode_g_w=2;
-                      mode_o_w=2;
-                      i++;
-
-                  }
-                  if(argv[1][i]=='x'){
-                      mode_u_x=1;
-                      mode_g_x=1;
-                      mode_o_x=1;
-                      i++;
-
-                  }
-              }
-           }
-
+      
 
         mode_u=mode_u_r+mode_u_w+mode_u_x;
         mode_g=mode_g_r+mode_g_w+mode_g_x;
@@ -472,6 +340,23 @@ while ((ent = readdir(dir)) != NULL) {
   }
 }   */
     
+
+    return 0;
+}
+
+int main(int argc,char *argv[]){ //, char*env
+    if(argc<3){ //chmod options permissions file_name
+                /*If no options are specified, chmod modifies the permissions of the file 
+                specified by file name to the permissions specified by permissions.
+                So it's possible to have only 3 arguments--->xmod, permissions, file_name*/
+        printf("Not enough arguments\n");
+        return 1;
+    }
+
+    if(xmod(argv)){
+        return 1;
+    }
+
     return 0;
 
 
