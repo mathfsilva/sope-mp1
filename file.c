ddfa69c9a899c43d76f21fd4e97ca23111a05821
melisilva <melissa.silva2001.ms@gmail.com>
2021-03-06 12:01:31 +0000

    Fixed files writing + added info for PROC_CREAT, need to think of way to add info for PROC_EXIT

--- file.c
+++ file.c
@@ -13,30 +13,34 @@ void eventHandler(int code, int argc, char *argv[], char*reg,double time_taken){
         case 0: {
             //printf("I'm here\n");
             char t[sizeof(time_taken)]; 
-            char msg[] = " PROCESS CREATED WITH PID ";
+            char const *msg = " PROCESS WITH PID ";
             static int size = sizeof(getpid())/sizeof(char);
             char pid[size];
-            printf("\n%d %d", getpid(), size);
+            //printf("\n%d %d", getpid(), size);
             snprintf(pid, size, "%d\n", getpid());
-            snprintf(t,500,"%f",time_taken);
+            snprintf(t,9,"%f",time_taken);
             write(fd,t,sizeof(t)-1);
-            write(fd, msg, sizeof(msg)-1);
+            write(fd, msg, strlen(msg));
             write(fd, pid,  sizeof(pid)-1);
+            for(int j=0;argv[j]!=NULL;j++){
+                write(fd," ",sizeof(" ")-1);
+                write(fd,argv[j],strlen(argv[j]));
+            }
             write(fd,"\n",sizeof("\n")-1);
             break;
         }
         case 1: {
             char t[sizeof(time_taken)]; 
-            char msg[] = " PROCESS WITH PID ";
-            char final[] = " TERMINATED\n";
+            char const *msg = " PROCESS WITH PID ";
+            char const *final= " TERMINATED\n";
             static size_t size = sizeof(getpid())/sizeof(char);
             char pid[size];
             snprintf(pid, size, "%d", getpid());
-            snprintf(t,500,"%f",time_taken);
+            snprintf(t,9,"%f",time_taken);
             write(fd,t,sizeof(t)-1);
-            write(fd, msg, sizeof(msg)-1);
+            write(fd, msg, strlen(msg));
             write(fd, pid, sizeof(pid)-1);
-            write(fd, final, sizeof(final)-1);
+            write(fd, final, strlen(final));
             close(fd);
             break;
         }
