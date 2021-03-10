f10adb40bd59fc36fd0c2535bacc8e70f8333cd5
melisilva <melissa.silva2001.ms@gmail.com>
2021-03-10 08:32:33 +0000

    Some input errors in files corrected + FILE_MODF done

--- file.c
+++ file.c
@@ -51,6 +51,7 @@ void eventHandler(int code, int argc, char *argv[], char*reg,double time_taken){
 void write_PROC_CREATE(int fd,char *argv[],double time_taken){
     char t[sizeof(time_taken)]; 
     char const *msg = " PROCESS WITH PID ";
+    char const *msg2 = " PROC_CREAT ";
     static int size = sizeof(getpid())/sizeof(char);
     char pid[size];
     //printf("\n%d %d", getpid(), size);
@@ -59,6 +60,7 @@ void write_PROC_CREATE(int fd,char *argv[],double time_taken){
     write(fd,t,sizeof(t)-1);
     write(fd, msg, strlen(msg));
     write(fd, pid,  sizeof(pid)-1);
+    write(fd,msg2,strlen(msg2));
     for(int j=0;argv[j]!=NULL;j++){
         write(fd," ",sizeof(" ")-1);
         write(fd,argv[j],strlen(argv[j]));
@@ -71,7 +73,7 @@ void write_PROC_EXIT(int fd,double time_taken,int exit_code){
     char exit[1]={'0'};
     exit[0]=exit_code +'0';
     char const *msg = " PROCESS WITH PID ";
-    char const *final= " TERMINATED WITH EXIT STATUS ";
+    char const *final= " PROC_EXIT WITH EXIT STATUS ";
     static size_t size = sizeof(getpid())/sizeof(char);
     char pid[size];
     snprintf(pid, size, "%d", getpid());
@@ -86,5 +88,24 @@ void write_PROC_EXIT(int fd,double time_taken,int exit_code){
 
 
 void write_FILE_MODF(int fd,double time_taken,char*old_mode,char* new_mode,char*file_name){ 
-    //printf("Holo\n");
+    char t[sizeof(time_taken)];
+    static size_t size = sizeof(getpid())/sizeof(char);
+    char const *msg=" FILE_MODF ";
+    char const *msg1 = " PROCESS WITH PID ";
+    char const *point=" : ";
+    char pid[size];
+    snprintf(pid, size, "%d", getpid());
+    snprintf(t,9,"%f",time_taken); 
+    write(fd,t,sizeof(t)-1);
+    write(fd,msg1,strlen(msg1));
+    write(fd,pid,sizeof(pid)-1);
+    write(fd," ",sizeof(" ")-1);
+    write(fd,file_name,strlen(file_name));
+    write(fd,point,strlen(point));
+    write(fd,old_mode,strlen(old_mode));
+    write(fd,point,strlen(point));
+    write(fd,new_mode,strlen(new_mode));
+    write(fd,"\n",sizeof("\n")-1);
+
+
 }
