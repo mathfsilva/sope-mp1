2f371f76d8eb103a9880fcf7113694c260006cf3
mathfsilva <mathfsilva14@gmail.com>
2021-03-17 20:06:19 +0000

    Updated signals.c to the cpplint standard!
    Going to add more updating to file.c in a second!

--- signals.c
+++ signals.c
@@ -1,4 +1,4 @@
-//Signals.c
+// Signals.c
 /*Okay so here's the plan (I think):
   1.In main, call function to subscribe SIGINT interruptions->using example ssignal as a
 base. struct sigaction new; new.sa_handler=signal_handler; (function that handles signals)
@@ -27,22 +27,20 @@ write_SIGNAL_SENT.
 
 #include "file.h"
 #include "traverse.h"
+#include "signals.h"
 #include "xmod.h"
 
-void sigint_handler(int signo)
-{
-
-  if (write_SIGNAL_RECV("SIGINT"))
-  {
+void sigint_handler(int signo) {
+  if (write_SIGNAL_RECV("SIGINT")) {
     perror("Failed to write to log file\n");
     write_PROC_EXIT(1);
     exit(1);
   }
 
-  size_t nbytes = snprintf(NULL, 0, "%d ; %s ; %d ; %d\n", getpid(), global_file_path, nftot, nfmod) + 1;
+  size_t nbytes = snprintf(NULL, 0, "%d ; %s ; %d ; %d\n",
+                            getpid(), global_file_path, nftot, nfmod) + 1;
 
-  if (nbytes < 0)
-  {
+  if (nbytes < 0) {
     perror("Failed snprintf\n");
     write_PROC_EXIT(1);
     exit(1);
@@ -50,105 +48,86 @@ void sigint_handler(int signo)
 
   char *str_final = malloc(nbytes);
 
-  snprintf(str_final, nbytes, "%d ; %s ; %d ; %d\n", getpid(), global_file_path, nftot, nfmod);
+  snprintf(str_final, nbytes, "%d ; %s ; %d ; %d\n",
+            getpid(), global_file_path, nftot, nfmod);
 
   printf("%s", str_final);
-  //printf("My current Childs pid is: %d\n", PID_CURRENT_CHILD);
+  // printf("My current Childs pid is: %d\n", PID_CURRENT_CHILD);
 
-  if (PID_CURRENT_CHILD == 0)
-  {
-    //sleep(5);
+  if (PID_CURRENT_CHILD == 0) {
+    // sleep(5);
     printf("Do you wish to end the program? (y/n)\n");
     char letter = getchar();
     int c;
-    while ((c = getchar()) != '\n' && c != EOF)
-    {
-    }
+    while ((c = getchar()) != '\n' && c != EOF) {}
 
-    if (letter == 'y')
-    {
+    if (letter == 'y') {
       printf("Ending the program\n");
       write_PROC_EXIT(1);
       exit(1);
-    }
-    else if (letter == 'n')
-    {
+    } else if (letter == 'n') {
       printf("Continuing the program\n");
-    }
-    else
-    {
+    } else {
       printf("Bad input continuing the program\n");
     }
   }
   free(str_final);
 }
 
-int subscribe_SIGINT()
-{
-
-  //printf("Subscribed SIGINT signal\n");
+int subscribe_SIGINT() {
+  // printf("Subscribed SIGINT signal\n");
   struct sigaction interruption;
   interruption.sa_handler = sigint_handler;
   interruption.sa_flags = 0;
-  if (sigemptyset(&interruption.sa_mask))
-  {
+
+  if (sigemptyset(&interruption.sa_mask)) {
     perror("sigemptyset failed - subscribe_SIGINT\n");
     return 1;
   }
-  if (sigaction(SIGINT, &interruption, NULL))
-  {
+
+  if (sigaction(SIGINT, &interruption, NULL)) {
     perror("sigaction failed - subscribe_SIGINT\n");
     return 1;
   }
   return 0;
 }
 
-void signals_handler(int signo){
-  switch(signo)
-  {
+void signals_handler(int signo) {
+  switch (signo) {
     case 1:
-         if (write_SIGNAL_RECV("SIGHUP"))
-         {
+         if (write_SIGNAL_RECV("SIGHUP")) {
           perror("Failed to write to log file\n");
          }
     case 3:
-         if (write_SIGNAL_RECV("SIGQUIT"))
-         {
+         if (write_SIGNAL_RECV("SIGQUIT")) {
           perror("Failed to write to log file\n");
          }
     case 10:
-         if (write_SIGNAL_RECV("SIGUSR1"))
-         {
+         if (write_SIGNAL_RECV("SIGUSR1")) {
           perror("Failed to write to log file\n");
          }
     case 11:
-         if (write_SIGNAL_RECV("SIGSEGV"))
-         {
+         if (write_SIGNAL_RECV("SIGSEGV")) {
           perror("Failed to write to log file\n");
          }
     case 12:
-         if (write_SIGNAL_RECV("SIGUSR2"))
-         {
+         if (write_SIGNAL_RECV("SIGUSR2")) {
           perror("Failed to write to log file\n");
          }
     case 13:
-         if (write_SIGNAL_RECV("SIGPIPE"))
-         {
+         if (write_SIGNAL_RECV("SIGPIPE")) {
           perror("Failed to write to log file\n");
          }
     case 14:
-         if (write_SIGNAL_RECV("SIGALRM"))
-         {
+         if (write_SIGNAL_RECV("SIGALRM")) {
           perror("Failed to write to log file\n");
          }
     case 15:
-         if (write_SIGNAL_RECV("SIGTERM"))
-         {
+         if (write_SIGNAL_RECV("SIGTERM")) {
           perror("Failed to write to log file\n");
          }
     case 17:
-         if (write_SIGNAL_RECV("SIGCHLD"))
-         {
+         if (write_SIGNAL_RECV("SIGCHLD")) {
           perror("Failed to write to log file\n");
          }
     default:
@@ -156,60 +135,59 @@ void signals_handler(int signo){
   }
 }
 
-int subscribe_osignals(){
+int subscribe_osignals() {
   struct sigaction interruption;
   interruption.sa_handler = signals_handler;
   interruption.sa_flags = 0;
-  if (sigemptyset(&interruption.sa_mask))
-  {
+  if (sigemptyset(&interruption.sa_mask)) {
     perror("sigemptyset failed - subscribe_osignals\n");
     return 1;
   }
 
-  if (sigaction(SIGHUP, &interruption, NULL))
-  {
+  if (sigaction(SIGHUP, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGQUIT, &interruption, NULL))
-  {
+
+  if (sigaction(SIGQUIT, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGUSR1, &interruption, NULL))
-  {
+
+  if (sigaction(SIGUSR1, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGUSR2, &interruption, NULL))
-  {
+
+  if (sigaction(SIGUSR2, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGSEGV, &interruption, NULL))
-  {
+
+  if (sigaction(SIGSEGV, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGPIPE, &interruption, NULL))
-  {
+
+  if (sigaction(SIGPIPE, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGALRM, &interruption, NULL))
-  {
+
+  if (sigaction(SIGALRM, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGTERM, &interruption, NULL))
-  {
+
+  if (sigaction(SIGTERM, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
-  if (sigaction(SIGCHLD, &interruption, NULL))
-  {
+
+  if (sigaction(SIGCHLD, &interruption, NULL)) {
     perror("sigaction failed - subscribe_osignals\n");
     return 1;
   }
+
   return 0;
 }
